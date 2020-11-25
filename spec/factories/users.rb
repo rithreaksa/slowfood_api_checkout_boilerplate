7de484d910dbf46c9fe98a39a8f98ae6e41086f2
Thomas <thomas@craftacademy.se>
2020-11-25 16:07:03 +0100

    adds model spec for user adds factory for user class

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,7 +1,9 @@
+# frozen_string_literal: true
+
 FactoryBot.define do
   factory :user do
-    email { "MyString" }
-    password { "MyString" }
-    password_confirmation { "MyString" }
+    email { 'user@mail.com' }
+    password { 'password' }
+    password_confirmation { 'password' }
   end
 end
